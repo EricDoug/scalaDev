f358bccfbc7ce3cd89d65e1b5d729f66a93f3301
EricDoug <liuxd6688@126.com>
2014-10-21 19:23:06 +0800

    fuctional object

---
+++ src/main/scala/chapter6/Rational.scala
@@ -0,0 +1,30 @@
+package chapter6
+
+/**
+ * Created by EricDoug on 14-10-21.
+ * 有理数(Rational number) ：n/d  n被称作分子(numerator)，d被称作分母(denominator)
+ */
+class Rational (n : Int,d : Int) {
+  //precondition before create an object
+  require( d != 0)
+  //最大公约数
+  private val g = gcd(n.abs,d.abs)
+  val numer : Int = n / g
+  val denom : Int = d / g
+  //辅助构造器
+  def this(n : Int) = this(n,1)
+  //重写toString方法
+  override def toString = numer + "/" + denom
+  //加
+  def +(that : Rational) : Rational = new Rational(numer * that.denom + that.numer * denom,denom * that.denom)
+  //乘
+  def *(that : Rational) : Rational = new Rational(numer * that.numer,denom * that.denom)
+  //判断大小
+  def lessThan(that : Rational) = this.numer * that.denom < that.numer * this.denom
+  //求最大
+  def max(that : Rational) = if(this.lessThan(that)) that else this
+  //隐式转换
+  implicit def intToRational(x : Int) = new Rational(x)
+  //greatest common divisor
+  private def gcd(a : Int,b : Int) : Int = if(b == 0) a else gcd(b , a % b)
+}
